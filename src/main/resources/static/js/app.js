b8884c7d2a16f77cce1b5b954497a90d8042d59c
Raymond Bourges <raymond.bourges@univ-rennes1.fr>
2014-06-21 12:13:18 +0200

    refactoring: ajout de vues + controllers dans un module

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,47 +1,21 @@
-var app = angular.module('exampleApp', ['restangular']);
+var app = angular.module('exampleApp', [
+    'restangular',
+    'ngRoute',
+    'appControllers'
+]);
 
-app.controller('PersonCtrl', function ($scope, $http, Restangular, token) {
-
-    Restangular.addResponseInterceptor(function (data, operation, what) {
-        //Restangular ne supporte pas encore HAL (https://github.com/mgonto/restangular/issues/661)
-        if (operation === 'getList') {
-            if(data._embedded) {
-                return data._embedded[what];
-            }
-            else {
-                return [];
-            }
-        }
-        return data;
-    });
-
-    Restangular.setDefaultHeaders({'X-CSRF-TOKEN': token});
-
-
-    //selfLink a la spring-data-rest
-    Restangular.setRestangularFields({
-        selfLink: '_links.self.href'
-    });
-
-    refreshPersons();
-
-    $scope.remove = function(person) {
-        person.remove().then(function() {
-            refreshPersons();
-        });
-    };
-
-    $scope.addSomePersons = function() {
-        Restangular.one('add').post().then(function() {
-            refreshPersons();
-        });
-    };
-
-    function refreshPersons() {
-        var personsUrl = Restangular.all('persons');
-        personsUrl.getList().then(function(persons) {
-            $scope.persons = persons;
-        });
-    }
-
-});
+app.config(['$routeProvider',
+    function($routeProvider) {
+        $routeProvider.
+            when('/persons', {
+                templateUrl: 'views/persons.html',
+                controller: 'PersonsController'
+            }).
+            when('/person', {
+                templateUrl: 'views/person.html',
+                controller: 'PersonController'
+            }).
+            otherwise({
+                redirectTo: '/persons'
+            });
+    }]);
---
+++ src/main/resources/static/js/controllers.js
@@ -0,0 +1,47 @@
+var appControllers = angular.module('appControllers', []);
+
+appControllers.controller('PersonsController', function ($scope, $http, Restangular, token) {
+
+    Restangular.addResponseInterceptor(function (data, operation, what) {
+        //Restangular ne supporte pas encore HAL (https://github.com/mgonto/restangular/issues/661)
+        if (operation === 'getList') {
+            if(data._embedded) {
+                return data._embedded[what];
+            }
+            else {
+                return [];
+            }
+        }
+        return data;
+    });
+
+    Restangular.setDefaultHeaders({'X-CSRF-TOKEN': token});
+
+
+    //selfLink a la spring-data-rest
+    Restangular.setRestangularFields({
+        selfLink: '_links.self.href'
+    });
+
+    refreshPersons();
+
+    $scope.remove = function(person) {
+        person.remove().then(function() {
+            refreshPersons();
+        });
+    };
+
+    $scope.addSomePersons = function() {
+        Restangular.one('add').post().then(function() {
+            refreshPersons();
+        });
+    };
+
+    function refreshPersons() {
+        var personsUrl = Restangular.all('persons');
+        personsUrl.getList().then(function(persons) {
+            $scope.persons = persons;
+        });
+    }
+
+});
